c329cd135aeca2aa535cc5e344440a6fa79ec07b
teruteru634 <miyanagateru634@gmail.com>
2020-07-17 16:42:28 +0900

    両開きのドアのギミックができたが

---
+++ Assets/Models/Door/DoorTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] Animator m_anim;
+    [SerializeField] string m_parameter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_anim = m_anim.gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            m_anim.SetTrigger(m_parameter);
+        }
+    }
+}
